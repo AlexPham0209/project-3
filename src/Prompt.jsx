ab451eede5bd757116625e288a739eb6680adc0f
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2025-05-03 16:36:29 -0400

    Make sure that there is an input in the prompt before showing the table

--- src/Prompt.jsx
+++ src/Prompt.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import { Container, Form, FormGroup, Row, Button } from 'react-bootstrap';
+import { propTypes } from 'react-bootstrap/esm/Image';
 // import "bootstrap/dist/css/bootstrap.css";
 
 export default function Prompt({setName, credits, setCredits}) {
+    const [properties, setProperties] = useState({
+        name: '',
+        credits: '',
+    });
+
     const onSubmit = (e) => {
         e.preventDefault();
         const {name, credits} = e.target;
@@ -10,19 +17,36 @@ export default function Prompt({setName, credits, setCredits}) {
         setCredits(Number(credits.value));
     }
 
+    const onNameChange = (e) => {
+        e.preventDefault();
+        setProperties({
+            name: e.target.value,
+            credits: properties.credits
+        });
+    }; 
+
+    const onCreditsChange = (e) => {
+        e.preventDefault();
+        setProperties({
+            name: properties.name,
+            credits: e.target.value
+        });
+    }; 
+
+    console.log(properties);
     return (
         <Form className='course-form' onSubmit={onSubmit}>
             <Form.Group className='text-bar' controlId='name'>
                 <Form.Label className='text-description'>Student Name</Form.Label>
-                <Form.Control className='text-input' type="name" placeholder='Enter Name' disabled={credits !== null}></Form.Control>
+                <Form.Control className='text-input' type="name" placeholder='Enter Name' disabled={credits !== null} onChange={onNameChange}></Form.Control>
             </Form.Group>
             
             <Form.Group className='text-bar' controlId='credits'>
                 <Form.Label className='text-description'>Total Transfer Credits</Form.Label>
-                <Form.Control className='text-input' type="name" placeholder='Enter Credits' disabled={credits !== null}></Form.Control>
+                <Form.Control className='text-input' type="number" placeholder='Enter Credits' disabled={credits !== null} defaultValue={0} onChange={onCreditsChange}></Form.Control>
             </Form.Group>
 
-            <Button type='submit' style={{display: 'none'}}>Submit</Button>
+            <Button type='submit' style={{display: 'none'}} disabled={properties.name === '' || properties.value === ''}>Submit</Button>
         </Form>
     );
 }
